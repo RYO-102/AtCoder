34a7b1ec78494b95a470da0556970692469a9481
RYO-102 <ryou_ryou@docomo.ne.jp>
2024-05-07 01:13:20 +0900

    upload EXes

---
+++ APG4b/EX11.cc
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int N, A;
+  cin >> N >> A;
+
+  string op;
+  int B;
+
+  for(int i = 0; i < N; i++){
+    cin >> op >> B;
+    if(op == "+"){
+        A += B;
+    }else if(op == "-"){
+        A -= B;
+    }else if(op == "*"){
+        A *= B;
+    }else{
+        if(B == 0){
+            cout << "error" << endl;
+            break;
+        }else{
+            A /= B;
+        }
+    }
+    //if(B == 0)
+        //break;
+    cout << i + 1 << ":" << A << endl;
+  }
+}
---
+++ APG4b/EX3.cc
@@ -0,0 +1,8 @@
+//#include <bits/stdc++.h>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << 100 * (100 + 1) / 2 << endl;
+}
