5fd6175255096da838005c00b9ec805e8a60b7ea
RYO-102 <ryou_ryou@docomo.ne.jp>
2024-12-11 22:37:18 +0900

    upload ABC014_B.cc

---
+++ ABC_B/ABC014_B.cc
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <map>
+#include <algorithm>
+#include <climits>
+#include <cmath>
+#include <iomanip>
+#include <bitset>
+#include <set>
+#include <regex>
+#include <cstdint>
+#include <stack>
+
+using namespace std;
+using ll = long long;
+using ull = unsigned long long;
+using ld = long double;
+using graph = vector<vector<int>>;
+
+#define rep(i, begin, limit) for(ll i = (ll)begin; i < (ll)limit; i++)
+#define irep(i, begin, limit) for(ll i = (ll)begin; i <= (ll)limit; i++)
+#define rrep(i, begin, limit) for(ll i = (ll)begin; i > (ll)limit; i--)
+#define rirep(i, begin, limit) for(ll i = (ll)begin; i >= (ll)limit; i--)
+#define all(vec_name) vec_name.begin(), vec_name.end()
+#define rall(vec_name) vec_name.rbegin(), vec_name.rend()
+
+//sort(S.bigin(), S.end(), greater<int>())
+//set<type> insert(n) erase(n) name.count(n)
+//map<type,type> [key]=n erase(key)
+//pair<type, type> .first .second
+//vector<vector<int>> area(H, vector<int>(W, 0));
+//順列 next_permutation(all(vector))
+
+//sort O(NlogN)
+//S.substr(開始位置, 取り出す長さ);
+//int 最大値 2,147,483,647
+//TLE目安 10^8がアウト
+
+int main(){
+	//ABC 014 B
+	int n, X;
+	cin >> n >> X;
+	vector<int> a(n);
+	rep(i, 0, n) cin >> a.at(i);
+
+	int ans = 0;
+
+	rep(i, 0, n){
+		if(X & (1 << i)) ans += a.at(i);
+	}
+
+	cout << ans << endl;
+
+	return 0;
+}
