32bcedefffcdd884b23e13497176fab62365b955
RYO-102 <ryou_ryou@docomo.ne.jp>
2024-12-17 23:55:48 +0900

    upload ABC365_C

---
+++ ABC_C/ABC365_C.cc
@@ -0,0 +1,79 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <map>
+#include <algorithm>
+#include <climits>
+#include <cmath>
+#include <iomanip>
+#include <bitset>
+#include <set>
+#include <regex>
+#include <cstdint>
+#include <queue>
+#include <stack>
+
+using namespace std;
+using ll = long long;
+using ull = unsigned long long;
+using ld = long double;
+using graph = vector<vector<int>>;
+
+#define rep(i, begin, limit) for(ll i = (ll)begin; i < (ll)limit; i++)
+#define irep(i, begin, limit) for(ll i = (ll)begin; i <= (ll)limit; i++)
+#define rrep(i, begin, limit) for(ll i = (ll)begin; i > (ll)limit; i--)
+#define rirep(i, begin, limit) for(ll i = (ll)begin; i >= (ll)limit; i--)
+#define all(vec_name) vec_name.begin(), vec_name.end()
+#define rall(vec_name) vec_name.rbegin(), vec_name.rend()
+
+//sort(S.bigin(), S.end(), greater<int>())
+//set<type> insert(n) erase(n) name.count(n)
+//map<type,type> [key]=n erase(key)
+//pair<type, type> .first .second
+//vector<vector<int>> area(H, vector<int>(W, 0));
+//順列 next_permutation(all(vector))
+
+//sort O(NlogN)
+//S.substr(開始位置, 取り出す長さ);
+//int 最大値 2,147,483,647
+//TLE目安 10^8がアウト
+
+//宣言部
+ll N, M;
+vector<ll> A;
+
+int main(){
+	//ABC365 C
+	cin >> N >> M;
+	A.resize(N);
+	rep(i, 0, N) cin >> A.at(i);
+
+	//無制限の判断
+	ll total = 0;
+	rep(i, 0, N) total += A.at(i);
+	if(total <= M){
+		cout << "infinite" << endl;
+		return 0;
+	}
+
+	//二分探索
+	ll ok = 0, ng = M + 1, sum_budget = 0;
+
+	while(abs(ok - ng) > 1){
+		ll mid = (ok + ng) / 2;
+
+		rep(i, 0, N){
+			if(A.at(i) <= mid) sum_budget += A.at(i);
+			else sum_budget += mid;
+		}
+
+		if(sum_budget <= M) ok = mid;
+		else ng = mid;
+
+		sum_budget = 0;
+	}
+
+	cout << ok << endl;
+
+	return 0;
+}
